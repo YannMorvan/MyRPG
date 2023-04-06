cd779b3b37c457f24d0f82906a27482486ef78bc
DiamonsGold <romain.hure@epitech.eu>
2023-04-06 16:14:21 +0200

    FEAT(<buttons>): create all buttons for menu.

--- src/init/create_pause_button.c
+++ src/init/create_pause_button.c
@@ -23,7 +23,7 @@ static void update_pause_button(UNUSED void *component, button_t *button)
 button_t *create_pause_button(game_t *game)
 {
     button_t *button = create_button(game->engine, (sfVector2f){0, 0},
-        "pause", "./assets/button_pause.png");
+        "pause", "./assets/buttons/pause.png");
     if (!button)
         return NULL;
     scale_character(button->character, (sfVector2f){2, 2});
