39aa4d4961eb0310cb99a8f8c054dd2dd45c4c65
DiamonsGold <romain.hure@epitech.eu>
2023-04-14 16:15:29 +0200

    BUILD(<Makefile>): fix build.

--- lib/engine/MakeList.mk
+++ lib/engine/MakeList.mk
@@ -5,7 +5,7 @@
 ## MakeList.mk
 ##
 
-DIR			+= ./engine/engine/
+DIR			+= engine/engine/
 SRC			+= $(addprefix $(lastword $(DIR)),\
 				create_engine.c		\
 				event_engine.c		\
@@ -15,14 +15,14 @@ SRC			+= $(addprefix $(lastword $(DIR)),\
 				destroy_engine.c	\
 				)
 
-DIR			+=	./engine/textures/
+DIR			+=	engine/textures/
 SRC			+=	$(addprefix $(lastword $(DIR)),\
 				add_texture.c		\
 				get_texture.c		\
 				destroy_texture.c	\
 				)
 
-DIR			+=	./engine/sprites/
+DIR			+=	engine/sprites/
 SRC			+=	$(addprefix $(lastword $(DIR)),\
 				add_sprite.c				\
 				add_sprite_and_texture.c	\
@@ -30,8 +30,8 @@ SRC			+=	$(addprefix $(lastword $(DIR)),\
 				destroy_sprite.c			\
 				)
 
-DIR			+= ./engine/character/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/character/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				create_character.c		\
 				move_character.c		\
 				scale_character.c		\
@@ -40,37 +40,37 @@ SRC			+= $(addprefix $(lastword $(DIR)),\
 				destroy_character.c		\
 				)
 
-DIR			+= ./engine/collider/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/collider/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				create_collider.c		\
 				update_collision.c		\
 				collider_set_type.c		\
 				)
 
-DIR			+= ./engine/button/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/button/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				create_button.c			\
 				update_buttons.c		\
 				destroy_button.c		\
 				)
 
-DIR			+= ./engine/window/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/window/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				clear_window.c			\
 				set_splash_screen.c		\
 				display_splash_screen.c	\
 				destroy_window.c		\
 				)
 
-DIR			+= ./engine/mouse/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/mouse/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				create_mouse.c		\
 				event_mouse.c		\
 				update_mouse.c		\
 				)
 
-DIR			+= ./engine/scene/
-SRC			+= $(addprefix $(lastword $(DIR)),\
+DIR			+=	engine/scene/
+SRC			+=	$(addprefix $(lastword $(DIR)),\
 				scene_is_open.c		\
 				destroy_scene.c		\
 				)
