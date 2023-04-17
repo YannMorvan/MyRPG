414c1191fa8f5e8cbc698c7279358efd9f41ec68
Florian Trillaud <florian.trillaud@epitech.eu>
2023-04-17 10:53:31 +0200

    CHG use heart & delete man & life bar

--- src/init/create_hud.c
+++ src/init/create_hud.c
@@ -10,30 +10,12 @@
 #include "my_rpg.h"
 #include "ice/memory.h"
 
-hud_t *init_mana_bar(engine_t *engine, hud_t *hud)
+hud_t *init_heart(hud_t *hud)
 {
-    sfVector2f pos = {20, 500};
-    sfVector2f size = {2, 3};
-    sfIntRect rect = {13, 54, 111, 8};
+    sfVector2f pos = {20, 490};
+    sfVector2f size = {2, 2};
+    sfIntRect rect = {0, 0, 16, 16};
 
-    hud->mana = add_sprite_and_texture(engine, "hud", "assets/hud.png");
-    if (!hud->mana)
-        return NULL;
-    sfSprite_setTextureRect(hud->mana->sprite, rect);
-    sfSprite_setScale(hud->mana->sprite, size);
-    sfSprite_setPosition(hud->mana->sprite, pos);
-    return hud;
-}
-
-hud_t *init_life_bar(engine_t *engine, hud_t *hud)
-{
-    sfVector2f pos = {20, 470};
-    sfVector2f size = {3, 3};
-    sfIntRect rect = {13, 47, 111, 8};
-
-    hud->life = add_sprite_and_texture(engine, "hud", "assets/hud.png");
-    if (!hud->life)
-        return NULL;
     sfSprite_setTextureRect(hud->life->sprite, rect);
     sfSprite_setScale(hud->life->sprite, size);
     sfSprite_setPosition(hud->life->sprite, pos);
@@ -44,9 +26,9 @@ hud_t *create_hud(engine_t *engine)
 {
     hud_t *hud = ice_calloc(1, sizeof(hud_t));
 
-    if (!hud)
+    hud->life = add_sprite_and_texture(engine, "hud", "assets/heart_full.png");
+    if (!hud || !hud->life)
         return NULL;
-    hud = init_life_bar(engine, hud);
-    hud = init_mana_bar(engine, hud);
+    hud = init_heart(hud);
     return hud;
 }
