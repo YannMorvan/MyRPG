4286b3f1b798d2588ca4a1022abfdc254d28c6ba
DiamonsGold <romain.hure@epitech.eu>
2023-04-19 22:58:26 +0200

    FEAT(<update_player.c>): handle wall with player.

--- src/scenes/game/player/update_player.c
+++ src/scenes/game/player/update_player.c
@@ -7,15 +7,28 @@
 
 #include "my_rpg/game.h"
 
+void undo_velocity(player_t *player)
+{
+    float fspeed = (float)player->speed;
+
+    if (player->velocity.x)
+        player->velocity.x = (player->velocity.x > 0) ? -fspeed : fspeed;
+    if (player->velocity.y)
+        player->velocity.y = (player->velocity.y > 0) ? -fspeed : fspeed;
+}
+
 void update_player(rpg_t *rpg)
 {
     player_t *player = GAME(rpg)->player;
+    sfBool collide = player->character->collider->collide == COLLIDER_WALL;
     list_node_t *node = list_get_node(rpg->engine->sprites,
         GAME(rpg)->player->character->sprite);
 
+    if (collide)
+        undo_velocity(player);
     move_delta_character(rpg->engine, player->character, player->velocity);
-    if (node) {
-        list_pop_node(rpg->engine->sprites, node);
-        list_add_node(rpg->engine->sprites, node);
-    }
+    if (collide)
+        player->velocity = (sfVector2f){0, 0};
+    list_pop_node(rpg->engine->sprites, node);
+    list_add_node(rpg->engine->sprites, node);
 }
